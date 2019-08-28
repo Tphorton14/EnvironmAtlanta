b306a9b3a7e53dc2d85ce91096b80f7dd30785e4
zscott92@gmail.com <zscott92@gmail.com>
2019-08-28 11:28:39 -0400

    current progress attempting to pull

--- public/news_api_config.js
+++ public/news_api_config.js
@@ -1,15 +1,12 @@
 require("dotenv").config({
-    path: ``,
+    path: "./env",
 });
 
 module.exports = {
     resolve: 'news_api_config',
     options: {
         credentials: {
-            "private_key": 185145ab685d4df1bd29d79b88c3cc79,
+            "private_key": '185145ab685d4df1bd29d79b88c3cc79',
         }
     }
 }
-
-
-const newsFeedAPI = 185145ab685d4df1bd29d79b88c3cc79;
