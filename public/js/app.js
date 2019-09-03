bee356ed849825b5a37d176d13e5d75704ebbb9f
Ravi Mantur <RaviMantur@RavisMac.local>
2019-09-03 15:05:34 -0400

    used google api for location and modified background for location

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,18 @@
-$("#go").on("click", function (event) {
-    event.preventDefault();
-    const city = $('#inputCity').val()
-    const zip = $('#inputZip').val()
-    $.post('/api/earth911', { zip: zip }, function (data) {
-        console.log(data);
-        data.forEach(el => {
-            $('#location').append(`<p>${el.description}</p>`)
+
+
+$("#location").hide();
+$(document).ready(function () {
+
+    $("#go").on("click", function (event) {
+        event.preventDefault();
+        $("#location").show();
+        const city = $('#inputCity').val()
+        const zip = $('#inputZip').val()
+        $.post('/api/earth911', { city: city, zip: zip }, function (data) {
+            console.log(data);
+            data.forEach(el => {
+                $('#location').append(`<p>${el.description} ${el.distance} miles away</p>`);
+            })
         })
-    })
+    });
 });
